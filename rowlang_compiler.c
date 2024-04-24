970451013a28d4caf31c679afd90612d80f616f5
Tommaso Cerruti <tommaso.cerruti@casavo.com>
2024-04-24 12:06:48 +0200

    structure of the compiler

--- rowlang_compiler.c
+++ rowlang_compiler.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define TAPE_SIZE 30000
 
-// Define token types
 typedef enum {
     PULL,
     RECOVER,
@@ -17,17 +15,54 @@ typedef enum {
     END_OF_FILE
 } TokenType;
 
-// Structure to represent a token
 typedef struct {
-    TokenType type;
-    int value; // For repetition count
+    TokenType type; 
+    int value;
 } Token;
 
-// Function to read the next token from the input source
+Token nextToken(FILE *);
+
+void emitCode(Token);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <input_file>", argv[0]);
+        return 1;
+    }
+    FILE *source = fopen(argv[1], "r");
+    if (!source) {
+        printf("Error: Unable to open input file.\n");
+        return 1;
+    }
+    printf("#include <stdio.h>\n\nint main() {\n");
+    printf("unsigned char tape[%d] = {0};\nunsigned char *memory = tape;\n", TAPE_SIZE);
+
+    int loopDepth = 0;
+    Token token;
+    while ((token = nextToken(source)).type != END_OF_FILE) {
+        if (token.type == LOOP_START) {
+            printf("int loop%d = 0;\n", loopDepth);
+            printf("while (*memory) {\n");
+            loopDepth++;
+        } else if (token.type == LOOP_END) {
+            printf("}\n");
+            loopDepth--;
+        } else {
+            emitCode(token);
+        }
+    }
+    if (loopDepth != 0) {
+        printf("Error: Unmatched loop brackets.\n");
+        return 1;
+    }
+    printf("\nreturn 0;\n}");
+    fclose(source);
+    return 0;
+}
+
 Token nextToken(FILE *source) {
     Token token;
     char c = fgetc(source);
-
     switch (c) {
         case 'P':
             token.type = PULL;
@@ -57,11 +92,8 @@ Token nextToken(FILE *source) {
             token.type = END_OF_FILE;
             break;
         default:
-            // Ignore unrecognized characters
             return nextToken(source);
     }
-
-    // Read repetition count if available
     if ((c = fgetc(source)) >= '0' && c <= '9') {
         ungetc(c, source);
         fscanf(source, "%d", &token.value);
@@ -69,11 +101,10 @@ Token nextToken(FILE *source) {
         ungetc(c, source);
         token.value = 1;
     }
-
     return token;
 }
 
-// Function to emit C code for a given RowLang token
+
 void emitCode(Token token) {
     switch (token.type) {
         case PULL:
@@ -101,49 +132,6 @@ void emitCode(Token token) {
             printf("}");
             break;
         default:
-            // Ignore other token types
             break;
     }
-}
-
-// Main function to compile RowLang source code
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <input_file>", argv[0]);
-        return 1;
-    }
-
-    FILE *source = fopen(argv[1], "r");
-    if (!source) {
-        printf("Error: Unable to open input file.\n");
-        return 1;
-    }
-
-    printf("#include <stdio.h>\n\nint main() {\n");
-    printf("unsigned char tape[%d] = {0};\nunsigned char *memory = tape;\n", TAPE_SIZE);
-
-    int loopDepth = 0;
-    Token token;
-    while ((token = nextToken(source)).type != END_OF_FILE) {
-        if (token.type == LOOP_START) {
-            printf("int loop%d = 0;\n", loopDepth);
-            printf("while (*memory) {\n");
-            loopDepth++;
-        } else if (token.type == LOOP_END) {
-            printf("}\n");
-            loopDepth--;
-        } else {
-            emitCode(token);
-        }
-    }
-
-    if (loopDepth != 0) {
-        printf("Error: Unmatched loop brackets.\n");
-        return 1;
-    }
-
-    printf("\nreturn 0;\n}");
-
-    fclose(source);
-    return 0;
 }
